0b88f2889b8e4210651d16d0e219dadb9deaf013
Irene-Mukii <renniequin@gmail.com>
2021-02-19 18:20:31 +0300

    Adds a LICENSE information document

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,63 @@
 console.log("It works");
+let femaleNames = [];
+let maleNames = [];
 
 
-function inputResults(event){
-  let outputTag = document.getElementById('output');
-  let date = document.getElementById('date').value
-  let gender = document.getElementById('gender').value
 
-  let myBirthDay = new DragEvent(date);
-  let day = myBirthDay.split(' ')[0];
+//inputForm.addEventListener("submit",(e)=> {
+  //e.preventDefault();
+//});
 
-  let message = `HEY!Seeing as you were born on ${day} and you are a ${gender}: `;
-  outputTag.innerHTML = message;  
+function inputResults(){
+ let outputTag = document.getElementById('output');
+ let date = document.inputform.date.value
+   let gender = document.getElementById('gender').value
 
-if (gender=== male){
-  if (day === monday){
-    return "Your Akan name is Kwasi"
-  }else if (day === tuesday){
-    return "Your Akan name is Kwadwo"
-  } else if (day === wednesday) {
-    return "Your Akan name is Kwabena"
-  } else if (day === thurday) {
-    return "Your Akan name is Kwaku"
-  } else if (day === friday) {
-    return "Your Akan name is Yaw"
-  } else if (day === saturday) {
-    return "Your Akan name is Kofi"
-  } else if (day === sunday) {
-    return "Your Akan name is Kwame"
-  }
+   let myBirthDay = new Date('date').toDateString();
+   let day = myBirthDay.split(' ')[0];
 
-}else {
-    if (day === monday) {
-      return "Your Akan name is Akosua"
-    } else if (day === tuesday) {
-      return "Your Akan name is Adwoa"
-    } else if (day === tuesday) {
-      return "Your Akan name is Abenaa"
-    } else if (day === tuesday) {
-      return "Your Akan name is Akua"
-    } else if (day === tuesday) {
-      return "Your Akan name is Yaa"
-    } else if (day === tuesday) {
-      return "Your Akan name is Afua"
-    } else if (day === tuesday) {
-      return "Your Akan name is Ama"
-    }
-    }
+   let message = `HEY!Seeing as you were born on ${day} and you are a ${gender}: `;
+   outputTag.innerHTML = message;  
+
+// if (gender=== male){
+//   if (day === monday){
+//     return "Your Akan name is Kwasi"
+//   }else if (day === tuesday){
+//     return "Your Akan name is Kwadwo"
+//   } else if (day === wednesday) {
+//     return "Your Akan name is Kwabena"
+//   } else if (day === thurday) {
+//     return "Your Akan name is Kwaku"
+//   } else if (day === friday) {
+//     return "Your Akan name is Yaw"
+//   } else if (day === saturday) {
+//     return "Your Akan name is Kofi"
+//   } else if (day === sunday) {
+//     return "Your Akan name is Kwame"
+//   }
+
+// }else {
+//     if (day === monday) {
+//       return "Your Akan name is Akosua"
+//     } else if (day === tuesday) {
+//       return "Your Akan name is Adwoa"
+//     } else if (day === tuesday) {
+//       return "Your Akan name is Abenaa"
+//     } else if (day === tuesday) {
+//       return "Your Akan name is Akua"
+//     } else if (day === tuesday) {
+//       return "Your Akan name is Yaa"
+//     } else if (day === tuesday) {
+//       return "Your Akan name is Afua"
+//     } else if (day === tuesday) {
+//       return "Your Akan name is Ama"
+//     }
+//     }
 }
 
 // confirming that input is valid
-function validateForm(){
+// function validateForm(){
 
-  if (date <= 0 || date > 31 && month <= 0 || month>12) {
-     alert("Please confirm the date and month is a valid one")}  
-}
+//   if (date <= 0 || date > 31 && month <= 0 || month>12) {
+//      alert("Please confirm the date and month is a valid one")}  
+// }
